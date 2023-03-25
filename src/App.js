844b2bd43d29ea65e6133d33deb4250a56da5f62
surajrai-code <surajraieng>
2023-03-25 10:49:32 +0530

    movie fetch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
+// import Footer from "./components/Layout/Footer";
+// import Header from "./components/Layout/Header";
 
-
-import Footer from "./components/Layout/Footer";
-import Header from "./components/Layout/Header";
-
-
-
-
+import MovieList from "./components/Movies/MovieList";
+import React, { useState } from "react";
 function App() {
- 
+  const [movies, setMovies] = useState([]);
+  const fetchMoviesHandler =( async () => {
+   
+      const response = await fetch("https://swapi.dev/api/films/");
+      const data = await response.json();
+      const loadMovies = [];
+      for (let key in data) {
+        loadMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadMovies);
+  });
+
   return (
-  
-<div>
+    <div>
+      <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      <MovieList movies={movies} />
+      {/* <Header/>
 
-<Header/>
-
-<Footer/>
-</div>
+<Footer/> */}
+    </div>
   );
 }
 
---
+++ src/components/Movies/Movie.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Movie = (props) => {
+  return (
+    <li key={props.id}>
+      <h2>{props.title}</h2>
+      <h3>{props.releaseDate}</h3>
+      <p>{props.openingText}</p>
+      <p>{props.id}</p>
+    </li>
+  );
+};
+
+export default Movie;
